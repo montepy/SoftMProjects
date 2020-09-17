a0a0e8b7337772b22d3663afb514d50292aacce5
epywa <epywang42gmail.com>
2020-09-17 09:48:37 -0400

    initial commit

---
+++ src/SongLib/SongLib.java
@@ -0,0 +1,16 @@
+package SongLib;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SongLib extends Application {
+	
+	public void start(Stage primaryStage) {
+		//overrides inherited start method
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
